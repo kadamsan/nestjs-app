5df9b9ce69c1d35d836dda42bd27b7f0cd9aa1a8
sandeep kadam <kadamsandeep.1984@gmail.com>
2022-05-20 12:16:30 +0530

    removed database module

--- src/database/database.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
-
-@Module({
-  controllers: [],
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
-})
-export class DatabaseModule {}
