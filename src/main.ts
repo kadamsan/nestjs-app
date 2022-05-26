25e9ed18f4b7668ec272a65e215c0f6201d5766a
sandeep kadam <kadamsandeep.1984@gmail.com>
2022-05-26 12:34:29 +0530

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -34,15 +34,18 @@ async function bootstrap() {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
-  // Log current url of app and documentation
-  let baseUrl = app.getHttpServer().address().address;
-  if (baseUrl === '0.0.0.0' || baseUrl === '::') {
-    baseUrl = 'localhost';
+
+  if (configService.get<string>('NODE_ENV') === 'development') {
+    // Log current url of app and documentation
+    let baseUrl = app.getHttpServer().address().address;
+    if (baseUrl === '0.0.0.0' || baseUrl === '::') {
+      baseUrl = 'localhost';
+    }
+    // eslint-disable-next-line prettier/prettier
+    const url = `http://${baseUrl}:${configService.get<number>('PORT')}`;
+    logger.log(`Listening to ${url}`);
   }
-  // eslint-disable-next-line prettier/prettier
-  const url = `http://${baseUrl}:${configService.get<number>('PORT')}`;
-  logger.log(`Listening to ${url}`);
-  logger.log(`API Documentation available at ${url}/docs`);
+  logger.log(`API Documentation available at ${await app.getUrl()}/docs`);
   logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
